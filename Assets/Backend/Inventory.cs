167c9596ac061ac676f2f1fac1babd0f1a19c2f4
Andy Wang <trollscience1589@gmail.com>
2024-02-08 20:46:08 -0500

    started on inventory.cs:

---
+++ Assets/Backend/Inventory.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic
+
+/*
+ * Author: Andy Wang
+ * Date: Feb 8 2024
+ * This class represents a household's inventory of items in storage.
+ */
+class Inventory {
+    private Dictionary<string, int> _items;
+
+    // Create a new empty inventory
+    public Inventory() {
+        _items = new Dictionary<string, int>();
+    }
+
+    // Adds an item to the inventory.
+    public void AddItem(string newItem) {
+
+    }
+
+    // Removes an item from the inventory
+    public void RemoveItem(string item) {
+        
+    }
+
+    // Gets the count of a certain item in the inventory
+    public void GetAmount(string item)  {
+        
+    }
+}

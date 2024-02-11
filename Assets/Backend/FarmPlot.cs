6068fa4fee7c0d80e22b3b7b2fb60a7732241c7a
Andy Wang <andy_wang02@hotmail.com>
2024-02-11 12:48:24 -0500

    added dummy farmplot class and shop scene

---
+++ Assets/Backend/FarmPlot.cs
@@ -0,0 +1,23 @@
+// PLACEHOLDER TO SILENCE ERRORS UNTIL REAL FARMPLOT CLASS IS MADE
+namespace Backend {
+    public class FarmPlot {
+        // PLACEHOLDER PLACEHOLDER PLACEHOLDER
+        // DELETE THIS LATER
+
+        public int GetYield() {
+            return 0;
+        }
+
+        public int GetYieldWeather() {
+            return 1;
+        }
+
+        public int GetFertilizer() {
+            return 0;
+        }
+
+        public int GetSeedType() {
+            return 0;
+        }
+    }
+}

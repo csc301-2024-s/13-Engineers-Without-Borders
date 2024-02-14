dccac7854466ce656c7f3234da7ce211cedc8658
ACER <hoathai.nguyen@mail.utoronto.ca>
2024-02-13 22:07:18 -0500

    Initialized GameState

---
+++ Assets/Backend/GameState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This class represents the States of the game
+ */
+namespace Backend
+{
+    public static class GameState
+    {
+        static int s_Year;
+        static int s_Phase;
+        static int s_WeatherIndex;
+        static Household s_Player;
+        static Household[] s_Households;
+
+        static void Initalize(Household Player)
+        {
+
+        }
+
+        static void AdvanceToPhaseOne()
+        {
+
+        }
+
+        static void AdvanceToPhaseTwo()
+        {
+
+        }
+
+        static void AdvanceToPhaseThree()
+        {
+
+        }
+    }
+}

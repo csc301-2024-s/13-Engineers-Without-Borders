d4a73eca883f1ea9b76f4d67da4ec18f5ba8ff58
ACER <hoathai.nguyen@mail.utoronto.ca>
2024-02-15 17:03:17 -0500

    Add a CanBeAssigned method and Finished Initialzed method

--- Assets/Backend/GameState.cs
+++ Assets/Backend/GameState.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 /*
  * This class represents the States of the game
@@ -15,8 +17,15 @@ namespace Backend
         public static Household s_Player;
         public static Household[] s_Households;
 
+        // Initialize the game
         public static void Initialize(Household Player)
         {
+            s_Player = Player;
+            s_Year = 1;
+            s_Phase = 0;
+            System.Random rand = new Random();
+            s_WeatherIndex = rand.Next(1, 6);
+            s_Households = new Household[] { Player };
 
         }
 

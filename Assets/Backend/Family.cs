57e87a027f9b47ebc515f30cf1af60b567a3421e
ACER <hoathai.nguyen@mail.utoronto.ca>
2024-02-16 16:51:02 -0500

    Built household selection GUI

--- Assets/Backend/Family.cs
+++ Assets/Backend/Family.cs
@@ -24,14 +24,14 @@ public class Family
         for(int i = 0; i < NumChildren; i++)
         {
             //Temporary name
-            int Age = Random.Range(0, 12);
-            Child child = new Child("A", FamilyName, Age);
+            //int Age = Random.Range(0, 12);
+            Child child = new Child("A", FamilyName, 0);
             Children.Add(child);
         }
         for (int i = 0; i < numAdults; i++)
         {
             //Temporary name
-            int Age = Random.Range(18, 81);
+            //int Age = Random.Range(18, 81);
             Adult adult = new Adult("B", FamilyName);
             Adults.Add(adult);
         }

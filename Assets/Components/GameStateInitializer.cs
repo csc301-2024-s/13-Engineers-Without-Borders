6b83e93be2d702da2bc099a9feb9941bdec4d068
Andy Wang <andy_wang02@hotmail.com>
2024-02-15 21:00:24 -0500

    idk how to safely remove this file, idk where the commit was so i just comented it out

--- Assets/Components/GameStateInitializer.cs
+++ Assets/Components/GameStateInitializer.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class GameStateInitializer : MonoBehaviour
-{
-    Household player = new(5000, "Test", 2, 2, 10);
+// public class GameStateInitializer : MonoBehaviour
+// {
+//     Household player = new(5000, "Test", 2, 2, 10);
 
-    //GameState.Initalize(player);
-    GameState.s_Player = player;
-    GameState.s_Households = new Household[] { player };
-    GameState.s_Year = 1;
-    GameState.s_Phase = 2;
-    GameState.s_WeatherIndex = 3;
-}
+//     //GameState.Initalize(player);
+//     GameState.s_Player = player;
+//     GameState.s_Households = new Household[] { player };
+//     GameState.s_Year = 1;
+//     GameState.s_Phase = 2;
+//     GameState.s_WeatherIndex = 3;
+// }

58e1c39a907ded21dc4ec709a3890cca3647752d
Andy Wang <andy_wang02@hotmail.com>
2024-03-03 19:56:25 -0500

    welcome buttons are now interactive

---
+++ Assets/Components/UIDocumentLoadSceneButton.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+// Original Author: Andy Wang
+// Put this on a UIDocument with buttons in it
+// This script makes it so all the buttons with class "load-scene-button" load a scene upon click
+public class UIDocumentLoadSceneButton : MonoBehaviour
+{
+    private UIDocument _uiDocument;
+    private VisualElement _root;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _uiDocument = GetComponent<UIDocument>();
+        _root = _uiDocument.rootVisualElement;
+
+        // query all buttons of class load-scene-button to handle their click
+        _root.Query<Button>(className: "load-scene-button").ForEach(btn => {
+            btn.clicked += HandleLoadSceneButton(btn.tooltip);
+        });
+    }
+
+    // Return action that, when called, loads scene to sceneName
+    Action HandleLoadSceneButton(string sceneName)
+    {
+        return () => {
+            SceneUtils.LoadScene(sceneName);
+        };
+    }
+}

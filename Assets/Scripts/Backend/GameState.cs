51d18b24bb1804d775f17dd58902eb6f76b4ebd8
Andy Wang <andy_wang02@hotmail.com>
2024-03-21 20:43:20 -0400

    added button icons and fixed starvation bug causing incorrect results to be reported

--- Assets/Scripts/Backend/GameState.cs
+++ Assets/Scripts/Backend/GameState.cs
@@ -10,7 +10,8 @@ namespace Backend
 {
     public static class GameState
     {
-        public static int s_Year    ;
+        public const int StartMoney = 500;  // how much money the player starts with
+        public static int s_Year;
         public static int s_Phase;
         public static int s_WeatherIndex;
         public static Household s_Player;
@@ -18,34 +19,36 @@ namespace Backend
         public static PopupManager PopupManagerInstance { get; private set; }
         public static Household[] s_PredefinedHouseholds = new Household[]
         {
-            new Household(500, "Madhar", 3, 2, 3),
-            new Household(500, "Bralch", 4, 2, 3),
-            new Household(500, "Rama", 4, 3, 4),
-            new Household(500, "Dulai", 4, 5, 4),
-            new Household(500, "Grewal", 4, 6, 5),
-            new Household(500, "Kahlon", 3, 2, 4),
-            new Household(500, "Aujla", 4, 2, 2),
-            new Household(500, "Sandha", 4, 3, 6),
-            new Household(500, "Kohli", 0, 2, 1),
-            new Household(500, "Gill", 2, 4, 7),
-            new Household(500, "Dhillon", 1, 2, 2),
-            new Household(500, "Singh", 2, 3, 3),
-            new Household(500, "Kapoor", 3, 5, 5),
-            new Household(500, "Bhatia", 2, 2, 2),
-            new Household(500, "Gupta", 3, 5, 7),
-            new Household(500, "Khan", 1, 3, 4)   // added to make it an even number
+            new Household(StartMoney, "Madhar", 3, 2, 3),
+            new Household(StartMoney, "Bralch", 4, 2, 3),
+            new Household(StartMoney, "Rama", 4, 3, 4),
+            new Household(StartMoney, "Dulai", 4, 5, 4),
+            new Household(StartMoney, "Grewal", 4, 6, 5),
+            new Household(StartMoney, "Kahlon", 3, 2, 4),
+            new Household(StartMoney, "Aujla", 4, 2, 2),
+            new Household(StartMoney, "Sandha", 4, 3, 6),
+            new Household(StartMoney, "Kohli", 0, 2, 1),
+            new Household(StartMoney, "Gill", 2, 4, 7),
+            new Household(StartMoney, "Dhillon", 1, 2, 2),
+            new Household(StartMoney, "Singh", 2, 3, 3),
+            new Household(StartMoney, "Kapoor", 3, 5, 5),
+            new Household(StartMoney, "Bhatia", 2, 2, 2),
+            new Household(StartMoney, "Gupta", 3, 5, 7),
+            new Household(StartMoney, "Khan", 1, 3, 4)   // added to make it an even number
         };
 
-        public static int _startMoney = 500;
-        public static string _familyName;
-        public static int _startingNumChildren = 0;
-        public static int _startingNumAdults = 0;
-        public static int _startingAcresOfLand = 0;
-        
+        private static int _startingAcresOfLand = 0;
+
         // Initialize the game
         public static void Initialize(Household Player)
         {
-            s_Player = Player;
+            // create a COPY of the player
+            // We kind of messed up by making the predefined households an array of objects
+            // because objects are passed by reference
+            // so starvation would mutate the player object
+            // It would be better to load the information from something like a JSON file and
+            // create a new Household object in the initialize method
+            s_Player = new(StartMoney, Player.Family.Name, Player.Family.GetChildrenAmount(), Player.Family.GetAdultAmount(), Player.Land.Plots.Count);
             s_Year = 0;
             s_Phase = 0;
             Random rand = new Random();
@@ -54,14 +57,8 @@ namespace Backend
 
             Market.Initialize();
 
-            _startMoney = Player.Money;
-            _familyName = Player.Family.Name;
-            _startingNumChildren = Player.Family.Children.Count;
-            _startingNumAdults = Player.Family.Adults.Count;
             _startingAcresOfLand = Player.Land.Plots.Count;
-
-
-    }
+        }
 
         // (Year 2+ only) Choose random village event and household event for each household
         //Read WorkshopSheets/Fate Seeds and Tools Cards.pdf
@@ -84,14 +81,15 @@ namespace Backend
                     Child child = s_Player.Family.Children[0];
                     string name = child.FirstName;
                     s_Player.Family.Children.RemoveAt(0);
-                    PopupManager.QueuePopup("Notice", $"{name} starved to death!", "R.I.P.");
+                    PopupManager.QueuePopup("Notice", $"Your child, {name}, starved to death!", "R.I.P.");
                     s_Player.Wheat += Child.Consumption;
-                } else if (s_Player.Family.GetAdultAmount() > 0)
+                }
+                else if (s_Player.Family.GetAdultAmount() > 0)
                 {
                     Adult adult = s_Player.Family.Adults[0];
                     string name = adult.FirstName;
                     s_Player.Family.Adults.RemoveAt(0);
-                    PopupManager.QueuePopup("Notice", $"{name} starved to death!", "R.I.P.");
+                    PopupManager.QueuePopup("Notice", $"An adult, {name}, starved to death!", "R.I.P.");
                     s_Player.Wheat += Adult.Consumption;
                 }
                 s_Player.Wheat = Math.Min(s_Player.Wheat, 0);  // don't let it go over 0
@@ -100,15 +98,6 @@ namespace Backend
             if (s_Player.Family.GetAdultAmount() <= 0)
             {
                 PopupManager.QueuePopup("Game Over!", $"All of your adults starved to death!", "Aw man");
-                // Readded all the children and adults to the family 
-                for (int i = 0; i < _startingNumChildren; i++)
-                {
-                    s_Player.Family.CreateChild();
-                }
-                for (int i = 0; i < _startingNumAdults; i++)
-                {
-                    s_Player.Family.CreateAdult();
-                }
                 SceneUtils.LoadScene("Results");
                 return;
             }
@@ -150,10 +139,11 @@ namespace Backend
                 }
             }
 
-            if (s_Year == 1) {
+            if (s_Year == 1)
+            {
                 AdvanceToPhaseTwo();
             }
-            else if (!s_Player.Inventory.Contains("Tubewell")) 
+            else if (!s_Player.Inventory.Contains("Tubewell"))
             {
                 PopupManager.QueuePopup("Notice", $"Irrigation skipped due to lack of tubewell", "Okay");
                 AdvanceToPhaseTwo();
@@ -177,7 +167,7 @@ namespace Backend
             {
                 household.RemoveLabour();
             }
-            
+
             if (s_Player.Wheat < 0)
             {
                 PopupManager.QueuePopup("Notice", "You need to buy wheat to feed your family! If you don't buy enough by the end of this year, some of your family will starve!", "Oh no!");
@@ -185,6 +175,8 @@ namespace Backend
 
             SceneUtils.LoadScene("Market");
         }
+
+        // For the results screen to report your final results at end of game
         public static Dictionary<string, int> ResultReport()
         {
             Dictionary<string, int> results = new Dictionary<string, int>();
@@ -192,7 +184,7 @@ namespace Backend
             //ending year
             results["end-of-year"] = s_Year;
             //starting savings
-            results["starting-savings"] = 500;
+            results["starting-savings"] = StartMoney;
             //starting acres of land
             results["starting-acres-of-land"] = _startingAcresOfLand;
             //tubewell
@@ -236,7 +228,7 @@ namespace Backend
             results["children-number"] = 0;
             if (s_Player != null)
             {
-                results["children-number"] = s_Player.Family.Adults.Count;
+                results["children-number"] = s_Player.Family.Children.Count;
             }
 
             return results;
